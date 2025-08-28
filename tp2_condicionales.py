3cf2151dfead602f40d521fe73ad27ad46a9e040
dfrison12 <dariohfrison@gmail.com>
2025-08-27 21:03:30 -0300

    Subo TP Unidad 2 : Condicionales - Ejercicio 2

--- tp2_condicionales.py
+++ tp2_condicionales.py
@@ -13,7 +13,19 @@ def ejercicio_1():
     else:
         print("Es menor de edad")
 
+# Ejercicio 2: Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+# mostrar por pantalla un mensaje que diga "Aprobado"; en caso contrario deberá mostrar el mensaje "Desaprobado".
+def ejercicio_2():
+    print("EJERCICIO 2")
+    
+    nota = float(input("Por favor, ingresa tu nota: "))
+    
+    if nota >= 6:
+        print("Aprobado")
+    else:
+        print("Desaprobado")
 
 
 # Ejecutar el ejercicios
 ejercicio_1()
+ejercicio_2()
