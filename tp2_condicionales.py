f1d711150c08deda9cc1fed2f7ceab719c1627e9
dfrison12 <dariohfrison@gmail.com>
2025-08-27 21:09:39 -0300

    Subo TP Unidad 2: Condicionales Ejercicio 4

--- tp2_condicionales.py
+++ tp2_condicionales.py
@@ -38,8 +38,27 @@ def ejercicio_3():
     else:
         print("Por favor, ingrese un número par")
 
+# Ejercicio 4: Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece:
+# ● Niño/a: menor de 12 años.
+# ● Adolescente: mayor o igual que 12 años y menor que 18 años.
+# ● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+# ● Adulto/a: mayor o igual que 30 años.
+def ejercicio_4():
+    print("EJERCICIO 4")
+    
+    edad = int(input("Por favor, ingresa tu edad: "))
+    
+    if edad < 12:
+        print("Niño/a")
+    elif edad >= 12 and edad < 18:
+        print("Adolescente")
+    elif edad >= 18 and edad < 30:
+        print("Adulto/a joven")
+    else:
+        print("Adulto/a")
 
 # Ejecutar el ejercicios
 ejercicio_1()
 ejercicio_2()
 ejercicio_3()
+ejercicio_4()
