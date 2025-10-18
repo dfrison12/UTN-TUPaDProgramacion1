2292a5071231df6a1743b6dd4a274d7c1e43d847
dfrison12 <dariohfrison@gmail.com>
2025-10-18 17:39:55 -0300

    Fix el servidor debe retornar un json en la respuesta

--- TPI AYSO/servidor/servidor.py
+++ TPI AYSO/servidor/servidor.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 
 def home():
-    return jsonify({"status": "Servidor está ativo"})
+    return jsonify({"status": "Servidor Flask: Activo"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
