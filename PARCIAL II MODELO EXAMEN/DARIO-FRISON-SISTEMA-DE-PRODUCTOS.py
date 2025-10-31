c4bcd01d7d1721af0c6183c2b977205a42a32ac1
dariofrison <dario.frison@aridosoftware.com>
2025-10-31 13:34:44 -0300

    Subo Parcial 2 : Sistema de Productos - Agregar Producto

--- PARCIAL II MODELO EXAMEN/DARIO-FRISON-SISTEMA-DE-PRODUCTOS.py
+++ PARCIAL II MODELO EXAMEN/DARIO-FRISON-SISTEMA-DE-PRODUCTOS.py
@@ -6,6 +6,7 @@ NOMBRE_ARCHIVO = 'productos.csv'
 
 def obtener_productos():
 
+    # Verificar si el archivo existe; si no, crearlo con encabezados
     if not os.path.exists(NOMBRE_ARCHIVO):
         with open (NOMBRE_ARCHIVO, 'w', newline='', encoding='utf-8') as archivo:
 
@@ -13,14 +14,39 @@ def obtener_productos():
             escritor.writeheader()
             return []
 
-
+    # Leer los productos del archivo CSV
     productos = []
-
     with open(NOMBRE_ARCHIVO, newline='', encoding='utf-8') as archivo:
         lector = csv.DictReader(archivo)
         for fila in lector:
             productos.append({'nombre': fila['nombre'], 'precio': float(fila['precio'])})
     return productos
+
+def escribir_producto(producto):
+    with open(NOMBRE_ARCHIVO, 'a', newline='', encoding='utf-8') as archivo:
+        escritor = csv.DictWriter(archivo, fieldnames=['nombre', 'precio'])
+        escritor.writerow(producto)
+
+def existe_producto(nombre):
+    productos = obtener_productos()
+    
+    for producto in productos:
+        if producto['nombre'].lower() == nombre.strip().lower():
+            return True
+
+    return False
+
+
+def validar_numero_positivo(valor):
+    # Verificar cantidad de puntos
+    if valor.count('.') > 1:
+        return False
+    
+    # Verificar si es un número válido
+    if not valor.replace('.', '').isdigit():
+        return False
+
+    return True
 
 
 def mostrar_productos():
@@ -30,6 +56,26 @@ def mostrar_productos():
     print('Nombre   Precio')
     for producto in productos:
         print(f"- {producto['nombre']}: ${producto['precio']}")
+
+
+def agregar_producto():
+    print('\nAgregar nuevo producto:')
+
+    # Solicitar datos del producto
+    nombre = input('Nombre del producto: ').strip()
+    if existe_producto(nombre):
+        print(f'El producto "{nombre}" ya existe. No se puede agregar duplicados.')
+        return
+    
+    precio = input('Precio del producto: ').strip()
+    if not validar_numero_positivo(precio):
+        print('Precio inválido.')
+        return
+    precio = float(precio)
+
+    # Agregar el producto al archivo CSV
+    escribir_producto({'nombre': nombre, 'precio': precio})
+    print(f'Producto "{nombre}" agregado con éxito.')
 
 
 def mostrar_menu():
@@ -49,7 +95,7 @@ def mostrar_menu():
             case '1':
                 mostrar_productos()
             case '2':
-                print('Agregar producto')
+                agregar_producto()
             case '3':
                 print('Editar precio de producto')
             case '4':
